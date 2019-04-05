3fafabd15e948ba34e64203383228f9bd706f414
Live session user <13516070@std.stei.itb.ac.id>
2019-04-05 10:26:51 +0000

    modified radix sort algorithm

--- src/mpi_radix_sort.cpp
+++ src/mpi_radix_sort.cpp
@@ -10,10 +10,6 @@
 #define MASTER 0
 using namespace std;
 
-double startT, stopT;
-int position(int *arr, int eff_size);
-
-//declare random array procedure
 void rng(int* arr, int n) {
     int seed = 13515070;
     srand(seed);
@@ -22,106 +18,119 @@ void rng(int* arr, int n) {
     }
 }
 
-//sorting procedure
-void sort(int *arr, int s)
-{
-	int eff_size, minpos;
-	int temp;
+  
+// A utility function to get maximum value in arr[] 
+int getMax(int arr[], int n) 
+{ 
+    int mx = arr[0]; 
+    for (int i = 1; i < n; i++) 
+        if (arr[i] > mx) 
+            mx = arr[i]; 
+    return mx; 
+} 
+  
+// A function to do counting sort of arr[] according to 
+// the digit represented by exp. 
+void countSort(int arr[], int n, int exp) 
+{ 
+    int output[n]; // output array 
+    int i, count[10] = {0}; 
+  
+    // Store count of occurrences in count[] 
+    for (i = 0; i < n; i++) 
+        count[ (arr[i]/exp)%10 ]++; 
+  
+    // Change count[i] so that count[i] now contains actual 
+    //  position of this digit in output[] 
+    for (i = 1; i < 10; i++) 
+        count[i] += count[i - 1]; 
+  
+    // Build the output array 
+    for (i = n - 1; i >= 0; i--) 
+    { 
+        output[count[ (arr[i]/exp)%10 ] - 1] = arr[i]; 
+        count[ (arr[i]/exp)%10 ]--; 
+    } 
+  
+    // Copy the output array to arr[], so that arr[] now 
+    // contains sorted numbers according to current digit 
+    for (i = 0; i < n; i++) 
+        arr[i] = output[i]; 
+} 
+  
+// The main function to that sorts arr[] of size n using  
+// Radix Sort 
+void mpi_radix_sort(int arr[], int n) 
+{ 
+    // Find the maximum number to know number of digits 
+    int m = getMax(arr, n); 
+  
+    // Do counting sort for every digit. Note that instead 
+    // of passing digit number, exp is passed. exp is 10^i 
+    // where i is current digit number 
+    for (int exp = 1; m/exp > 0; exp *= 10) 
+        countSort(arr, n, exp); 
+} 
+  
+void print(int arr[], int n) 
+{ 
+    for (int i = 0; i < n; i++) 
+        cout << arr[i] << "\n"; 
+} 
+  
+int main(int argc, char **argv) 
+{ 
+    double start,stop; //waktu
+    int n = atoi(argv[1]); //jumlah elemen
+    int p,i,j; //p = jumlah thread, i/j = counter
+    int arr[n]; //array elemen
+    int my_rank;
+    MPI_Status status;
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&p);
+    
+    if(my_rank == 0) {
 	
-	for(eff_size = s; eff_size > 1; eff_size--) {
-		minpos = position(arr, eff_size);
-		temp = arr[minpos];
-		arr[minpos] = arr[eff_size-1];
-		arr[eff_size-1] = temp;
-	}
-}
-
-//define position function
-int position(int *arr, int eff_size)
-{
-	int i, min = 0;
-	
-	for(i=0; i<eff_size; i++)
-		min = arr[i] > arr[min] ? i: min;
-	return min;
-}
-
-
-main(int argc, char **argv)
-{
-	int * arr; //array elemen
-	int * local_array; //array lokal
-	int n = atoi(argv[1]); //jumlah elemen dalam array
-	int s; //jumlah elemen dalam array lokal = n/p
-	int p; // jumlah processor
-	int my_rank; //rank
-	int i,j; //counter
-
-	MPI_Status status;
-
-	MPI_Init(&argc,&argv);
-	MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
-	MPI_Comm_size(MPI_COMM_WORLD,&p);
-
-	//declare input array + file
-	if(my_rank==0)
-	{
-		//initial array
-		arr = (int *)malloc(n*sizeof(int));
-		rng(arr,n);
-		s = n/p;
-		for (j=0; j<p; j++) {
-			for (i=0; i<s; i++){
-				arr[j*s+i] = arr[j*s+i]%(2*n/p)+(j*2*n/p);
-			}
+	//init array
+    	rng(arr,n);
+	int s = n/p;
+	for (j=0; j<p; j++) {
+		for (i=0; i<s; i++){
+			arr[j*s+i] = arr[j*s+i]%(2*n/p)+(j*2*n/p);
 		}
-
-		//external input file
-		FILE * fout;
-		fout = fopen("input","w");
-		for(i=0;i<n;i++)
-			fprintf(fout,"%d\n",arr[i]);
-		fclose(fout);
 	}
 
-	//start timer
-	startT = MPI_Wtime();
+	//external input file
+	FILE * fout;
+	fout = fopen("input","w");
+	for(i=0;i<n;i++)
+		fprintf(fout,"%d\n",arr[i]);
+	fclose(fout);
+    }
 
-	//define local array
-	s = n/p;
-	local_array = (int*)malloc(s*sizeof(int));
+    //init array lokal
+    int s = n/p; //jumlah elemen dalam array lokal
+    int l_arr[s]; //array lokal
+    
+    start = MPI_Wtime();
 
-	//split to threads
-	MPI_Scatter(arr, s, MPI_INT, local_array, s, MPI_INT, 0, MPI_COMM_WORLD);
+    //proses radix sort
+    MPI_Scatter(arr, s, MPI_INT, l_arr, s, MPI_INT, 0, MPI_COMM_WORLD);
+    mpi_radix_sort(l_arr, s);
+    MPI_Gather(l_arr, s, MPI_INT, arr, s, MPI_INT, 0, MPI_COMM_WORLD);
 
-	//sorting on each thread
-	sort(local_array, s);
-
-	//recombine sorted arrays on threads
-	MPI_Gather(local_array, s, MPI_INT, arr, s, MPI_INT, 0, MPI_COMM_WORLD);
-
-	//stop timer
-	stopT = MPI_Wtime();
-
-	//print time + result array
-	if(my_rank==0)
-	{
-
-		//time
-		printf("%d threads for %d elements: %f secs\n", p, n, (stopT-startT));
-		
-		//result array output
-		FILE * fout;
-		fout = fopen("output","w");
-		for(i=0;i<n;i++)
-			fprintf(fout,"%d\n",arr[i]);
-		fclose(fout);
-	}
-
-	//free memory
-	free(local_array);
-	if (my_rank==0)
-		free(arr);
-
-	MPI_Finalize();
-}
+    stop = MPI_Wtime();
+    
+    if (my_rank == 0) {
+	//result array output
+	FILE * fout;
+	fout = fopen("output","w");
+	for(i=0;i<n;i++)
+		fprintf(fout,"%d\n",arr[i]);
+	fclose(fout); 
+	printf("time for radix sort for %d elements by %d processes: %f seconds\n",n,p,(stop-start));
+    }
+    MPI_Finalize();
+} 
